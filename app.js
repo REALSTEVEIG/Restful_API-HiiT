b6d71985b40777fbfb24cc40468ebea80eb9d3a4
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-11-23 12:01:42 +0100

    docs

--- app.js
+++ app.js
@@ -6,9 +6,34 @@ const port = process.env.PORT || 8000
 const connectDB = require('./db/connect')
 const customersRoute = require('./routes/customer')
 const productsRoute = require('./routes/products')
+
+//API SECURITY MODULES
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const cors = require('cors')
+const xss = require('xss-clean')
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+app.use(helmet());
+app.use(cors())
+app.use(xss())
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour",
+});
+
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
+app.use('/getdocs', (req, res) => {
+    res.send('<h1>VISIT MY BLOG API VIA THE LINK BELOW</h1><a href="/api-docs">Documentation</a>')
+  })
+  
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use('/api/v1', customersRoute)
 app.use('/api/v1', productsRoute)
 
