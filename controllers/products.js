f59ecca4a4b62c5d9903c7c0cad81c2de445c86b
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-11-28 21:46:47 +0100

    query adjusted

--- controllers/products.js
+++ controllers/products.js
@@ -79,6 +79,16 @@ exports.getAllProduct = async (req, res) => {
             result = result.sort(sortList)
         }
 
+        else {
+            result = result.sort('product_name')
+        }
+
+        const page = req.query.page || 1
+        const limit = req.query.limit || 5
+        const skip = (page - 1) * limit
+
+        result = result.skip(skip).limit(limit)
+
         const products = await result
     
         return res.status(200).json({total : products.length, products})
